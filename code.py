a1eeb435807862ab67147e5f16920136798f6c2a
Rajesh Khangar <rkhangar1998@gmail.com>
2023-12-08 22:48:19 +0530

    changes done

--- code.py
+++ code.py
@@ -1,2 +1,2 @@
 print("Manali and Rajesh is perfect match for eachother bot are understanding humans")
-print("Manali and Rajesh is perfect match for eachother bot are understanding humans")
+print("Manali and Rajesh is perfect match for eachother both are understanding humans and lovers")
